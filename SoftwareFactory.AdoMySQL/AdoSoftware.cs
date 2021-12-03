03383b2257231bc000e9dcfa949b11cbe79db4d4
alexisX1 <alexis.eze.13@gmail.com>
2021-12-03 10:28:26 -0300

    as

--- SoftwareFactory.AdoMySQL/AdoSoftware.cs
+++ SoftwareFactory.AdoMySQL/AdoSoftware.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections.Generic;
-using et12.edu.ar.AGBD.Ado;
+using SoftwareFactory.AdoMySQL.Mapeadores;
+using SoftwareFactory.Core;
+using SoftwareFactory.Core.Ado;
 
 namespace SoftwareFactory.AdoMySQL
 {
-    public class AdoSoftwareFactory
+
+    public class AdoSoftwareFactory: IAdo
     {
         public AdoAGBD Ado { get; set; }
         public MapCliente MapCliente { get; set; }
@@ -12,7 +15,12 @@ namespace SoftwareFactory.AdoMySQL
             Ado = ado;
             MapCliente = new MapCliente(Ado);
         }
-        public void AltaCliente(Cliente cliente) => MapCliente.AltaCliente(Cliente);
+        public new void AltaCliente(Cliente cliente) => MapCliente.AltaCliente(cliente);
         public List<Cliente> ObtenerClientes() => MapCliente.ObtenerClientes();
+
+        public List<Cliente> ObtenerCliente()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
--- SoftwareFactory.AdoMySQL/MapCliente.cs
+++ SoftwareFactory.AdoMySQL/MapCliente.cs
@@ -1,43 +1,64 @@
-using et12.edu.ar.AGBD.Mapeadores;
-using et12.edu.ar.AGBD.Ado;
 using System;
-using System.Data;
 using System.Collections.Generic;
-using cliente;
+using SoftwareFactory.Core;
+using et12.edu.ar.AGBD.Ado;
+using et12.edu.ar.AGBD.Mapeadores;
+using System.Data;
 
 namespace SoftwareFactory.AdoMySQL.Mapeadores
 {
     public class MapCliente: Mapeador<Cliente>
     {
+
         public MapCliente(AdoAGBD ado):base(ado)
         {
             Tabla = "Cliente";
         }
-        public override Cliente ObjetoDesdeFila(DataRow fila)
-            => new Cliente()
-            {
-                Id = Convert.ToByte(fila["cuit"]),
-                Nombre = fila["RazonSocial"].ToString()
-            };
+
+        internal List<Cliente> ObtenerClientes()
+        {
+            throw new NotImplementedException();
+        }
 
         public void AltaCliente(Cliente cliente)
-            => EjecutarComandoCon("altaCliente", ConfigurarAltaCliente, PostAltaCliente, cliente);
+            => EjecutarComandoCon("altaCliente", ConfigurarAltaCliente, postAltaCliente, cliente);
+
+        private void EjecutarComandoCon(string v, Action<Cliente> configurarAltaCliente, object postAltaCliente, Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
 
         public void ConfigurarAltaCliente(Cliente cliente)
         {
             SetComandoSP("altaCliente");
 
             BP.CrearParametroSalida("unCuit")
-            .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Int32)
-            .SetValor(cliente.Cuit)
-            .AgregarParametro();
+                .SetTipo(MySql.Data.MySqlClient.MySqlDbType.Int32)
+                .SetValor(cliente.Cuit)
+                .AgregarParametro();
 
             BP.CrearParametro("unRazonSocial")
-            .SetTipoVarchar(50)
-            .SetValor(cliente.RazonSocial)
-            .AgregarParametro();
+                .SetTipoVarchar(50)
+                .SetValor(cliente.RazonSocial)
+                .AgregarParametro();
+        }
+
+        private object SetComandoSP(string v)
+        {
+            throw new NotImplementedException();
         }
 
         public List<Cliente> ObtenerCliente() => ColeccionDesdeTabla();
+
+        private List<Cliente> ColeccionDesdeTabla()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Cliente ObjetoDesdeFila(DataRow fila)
+        {
+            throw new NotImplementedException();
+        }
     }
+
 }
