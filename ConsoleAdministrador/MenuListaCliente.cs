32c5787fb6f904d3c7bc57eae3806c5f741a54b2
alexisX1 <alexis.eze.13@gmail.com>
2021-12-15 17:04:12 -0300

    ale

--- ConsoleAdministrador/MenuListaCliente.cs
+++ ConsoleAdministrador/MenuListaCliente.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Collections.Generic;
 using SoftwareFactory.Core;
+using et12.edu.ar.MenuesConsola;
 
 namespace ConsoleAdministrador.Menu
 {
-    public class MenuListaCliente: MenuLista<Cliente>
+    public abstract  class MenuListaCliente: MenuListador<Cliente>
     {
-        public string Nombre { get; internal set; }
 
         public override void imprimirElemento(Cliente cliente) => Console.WriteLine($"RazonSocial: {cliente.RazonSocial} - Cuit: {cliente.Cuit} ");
         public override List<Cliente> obtenerlista() =>  Program.Ado.obtenerCliente();
-        }
+        
     }
 }
